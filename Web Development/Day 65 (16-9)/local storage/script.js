de2157cbfaa75d2881388cf5e915c87c6efc8b5e
kuldeep464 <kuldeeppoonia464@gmail.com>
2024-09-20 14:46:20 +0530

    day 65

---
+++ Web Development/Day 65 (16-9)/local storage/script.js
@@ -0,0 +1,53 @@
+let form = document.querySelector("form")
+let tbody=document.querySelector("tbody")
+let clearStorage=document.querySelector("#clearstorage")
+let particularDelete=document.querySelector("#particularDelete")
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    let userOldData = JSON.parse(localStorage.getItem("user")) ?? []
+    let userobj ={
+        name:event.target.name.value,
+        email:event.target.email.value,
+        phone:event.target.phone.value
+    }
+    userOldData.push(userobj)
+    localStorage.setItem("user", JSON.stringify(userOldData))
+    console.log(userOldData)
+    event.target.reset()
+        userlist()
+})
+
+let userlist =()=>{
+    let rows;
+    tbody.innerHTML=''
+    let userOldData = JSON.parse(localStorage.getItem("user")) ?? []
+    userOldData.forEach((element,index) => {
+        rows+= ` <tr>
+                    <td>${index + 1}</td>
+                    <td>${element.name}</td>
+                    <td>${element.email}</td>
+                    <td>${element.phone}</td>
+                    <td><button onclick=deleteparticular(${index})>Delete</button></td>
+                    <td>Edit</td>
+                </tr>`
+        tbody.innerHTML=rows
+    });
+
+}
+userlist()
+
+clearStorage.addEventListener("click",()=>{
+    let userOldData = JSON.parse(localStorage.getItem("user")) ?? []
+    localStorage.removeItem("user")
+    userlist()
+
+})
+
+function deleteparticular (index){
+    console.log(index)
+    let userOldData = JSON.parse(localStorage.getItem("user")) ?? []
+    userOldData.splice(index,1)
+    localStorage.setItem("user", JSON.stringify(userOldData))
+    userlist()
+}
