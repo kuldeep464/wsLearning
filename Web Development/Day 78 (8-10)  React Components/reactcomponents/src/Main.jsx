17394f33cceb3ae60a8a9e8152559b74f0dc8222
kuldeep464 <kuldeeppoonia464@gmail.com>
2024-10-10 17:26:05 +0530

    NetflixHome

---
+++ Web Development/Day 78 (8-10)  React Components/reactcomponents/src/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+export default function () {
+  return (
+    <div>
+        <Header/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore impedit aspernatur dolores repellat accusantium cumque animi maxime dignissimos amet sint fugit hic laboriosam tenetur neque, doloribus error cupiditate optio nihil deserunt asperiores, voluptate quia eaque quasi in! Odio iusto iste dignissimos, quisquam velit amet ipsam?</p>
+        <Footer/>
+    </div>
+  )
+}
